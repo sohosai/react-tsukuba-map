bc59ff7cb4d480cd9f6d7d48331d97c8cb6386e1
itsu-dev <itsu020402@gmail.com>
2023-10-16 15:13:41 +0900

    fix: fix building bug

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    emptyOutDir: false,
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
-      name: 'index',
-      fileName: 'index',
+      name: 'ReactTsukubaMap',
+      fileName: 'react-tsukuba-map',
+    },
+    rollupOptions: {
+      external: ['react'], // バンドルしたくない依存関係を指定
+      output: {
+        globals: {
+          react: 'React', // UMDビルド時に、external指定した依存関係をscript タグで読み込まれた場合に使用される変数名を指定
+        },
+      },
     },
   },
 })
